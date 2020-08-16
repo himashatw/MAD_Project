7c996b4269dd974283d4dde474e45cb2cee8cc48
TOSHIBA <ushanjayasooriya@gmail.com>
2020-08-16 20:22:47 +0530

    commit from uj

--- app/src/main/java/com/example/shoppingez/TetsActivity.java
+++ app/src/main/java/com/example/shoppingez/TetsActivity.java
@@ -9,6 +9,6 @@ public class TetsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tets);
+        setContentView(R.layout.enquiry2);
     }
 }

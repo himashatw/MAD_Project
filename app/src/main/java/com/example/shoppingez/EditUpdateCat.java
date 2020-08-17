54bcbd1c2a055ddd391f2dc07b73af174d16999c
Punsisi Perera <punsisi.subhani@gmail.com>
2020-08-17 11:56:31 +0530

    ui from sisi

---
+++ app/src/main/java/com/example/shoppingez/EditUpdateCat.java
@@ -0,0 +1,14 @@
+package com.example.shoppingez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUpdateCat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_update_cat);
+    }
+}

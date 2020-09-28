07170c4bc8e593b3b1adbe2bbf41e9622ec5453b
TOSHIBA <ushanjayasooriya@gmail.com>
2020-09-28 11:05:20 +0530

    Test Ushan 11.05AM

--- app/src/main/java/com/example/shoppingez/Github.java
+++ app/src/main/java/com/example/shoppingez/Github.java
@@ -3,9 +3,10 @@ package com.example.shoppingez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Github extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
